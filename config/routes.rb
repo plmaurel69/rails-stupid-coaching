9b40191209aad8540fd4280ebaa31d4b83c6ad16
plmaurel69 <88190008+plmaurel69@users.noreply.github.com>
2021-12-07 21:26:11 +0100

    green

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # verb 'path', to: 'controller#action'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'questions#question'
+  get 'answer', to: 'questions#answer'
 end
